13d4cf0d15bb31e81bd6d1bcda4f2ca5b02feea9
Mark Daniel  Delgado <markpnoi26@gmail.com>
2020-01-15 22:33:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function merge(arrayOne, arrayTwo) {
   let sorted = []
   
   while (arrayOne.length != 0 && arrayTwo.length != 0) {
-    if (arrayOne[0] > arrayTwo[0]) {
-      sorted.push(findMinAndRemoveSorted(arrayTwo))
-    } else if (arrayOne[0] < arrayTwo[0]) {
+    if (arrayOne[0] < arrayTwo[0]) {
       sorted.push(findMinAndRemoveSorted(arrayOne))
+    } else if (arrayOne[0] > arrayTwo[0]) {
+      sorted.push(findMinAndRemoveSorted(arrayTwo))
     } 
   }
   return sorted
