e3206457e731cb2d51be7393395c49164241b0a7
Mark Daniel  Delgado <markpnoi26@gmail.com>
2020-01-15 22:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function findMinAndRemoveSorted(array) {
 function merge(arrayOne, arrayTwo) {
   let sorted = []
   
-  while (arrayOne.length !== 0 && arrayTwo.length !== 0) {
+  while (arrayOne.length != 0 && arrayTwo.length != 0) {
     if (arrayOne[0] > arrayTwo[0]) {
       sorted.push(findMinAndRemoveSorted(arrayTwo))
     } else if (arrayOne[0] < arrayTwo[0]) {
